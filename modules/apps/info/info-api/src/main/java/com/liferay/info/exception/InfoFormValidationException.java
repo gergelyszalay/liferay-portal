589850cbf5de168be72763ddb4777099388e62f5
Gergely Szalay <gergely.szalay@liferay.com>
2026-03-05 11:57:01 +0100

    LPD-78934 handle AssetCategoryException by throwing RequiredCategory

--- modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormValidationException.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormValidationException.java
@@ -5,6 +5,8 @@
 
 package com.liferay.info.exception;
 
+import com.liferay.asset.kernel.exception.AssetCategoryException;
+import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.captcha.CaptchaException;
 import com.liferay.portal.kernel.exception.InfoFormException;
@@ -288,6 +290,38 @@ public class InfoFormValidationException extends InfoFormException {
 			return LanguageUtil.format(
 				locale, "the-x-is-invalid", fieldLabel, false);
 		}
+
+	}
+
+	public static class RequiredAssetCategory
+		extends InfoFormValidationException {
+
+		public RequiredAssetCategory(
+			AssetVocabulary assetVocabulary,
+			AssetCategoryException assetCategoryException) {
+
+			_assetVocabulary = assetVocabulary;
+			_assetCategoryException = assetCategoryException;
+		}
+
+		public AssetCategoryException getAssetCategoryException() {
+			return _assetCategoryException;
+		}
+
+		public AssetVocabulary getAssetVocabulary() {
+			return _assetVocabulary;
+		}
+
+		@Override
+		public String getLocalizedMessage(Locale locale) {
+			return LanguageUtil.format(
+				locale, "please-select-at-least-one-category-for-x",
+				(_assetVocabulary != null) ? _assetVocabulary.getTitle(locale) :
+					StringPool.BLANK);
+		}
+
+		private final AssetCategoryException _assetCategoryException;
+		private final AssetVocabulary _assetVocabulary;
 
 	}
 
